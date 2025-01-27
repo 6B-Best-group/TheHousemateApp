2938d47f142b6b77fcd108b791a45795d25c25f7
AWell35 <up2196335@myport.ac.uk>
2025-01-27 13:35:58 +0000

    Creation of a log in form front end

--- housemate_app/lib/log_it.dart
+++ housemate_app/lib/log_it.dart
@@ -10,6 +10,33 @@ class log_In extends StatefulWidget {
 class _log_inState extends State<log_In> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log In"),
+      ),
+      body: Form(
+        child: ListView(
+          children: [
+            TextField(decoration: InputDecoration()),
+            TextField(decoration: InputDecoration()),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.check_rounded),
+              label: Text("Log in!"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.question_mark),
+              label: Text("Forgot Password"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text("Make an account"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
