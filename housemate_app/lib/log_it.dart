f6996bf4b4e708754958d45aec762ae6f1e51f0a
AWell35 <up2196335@myport.ac.uk>
2025-01-27 12:51:26 +0000

    creating log in page

---
+++ housemate_app/lib/log_it.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class log_In extends StatefulWidget {
+  const log_In({super.key});
+
+  @override
+  State<log_In> createState() => _log_inState();
+}
+
+class _log_inState extends State<log_In> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
