9b2da9357f8d5ca1f3488a84c01361487de37214
AWell35 <up2196335@myport.ac.uk>
2025-03-03 11:15:42 +0000

    saving a quick placeholder sign up page

---
+++ housemate_app/lib/sign_up.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class log_In extends StatefulWidget {
+  const log_In({super.key});
+
+  @override
+  State<log_In> createState() => _log_inState();
+}
+
+class _log_inState extends State<log_In> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log In"),
+      ),
+      body: Form(
+        child: ListView(
+          children: [
+            const TextField(decoration: InputDecoration()),
+            const TextField(decoration: InputDecoration()),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.check_rounded),
+              label: Text("Log in!"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.question_mark),
+              label: Text("Forgot Password"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text("Make an account"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
