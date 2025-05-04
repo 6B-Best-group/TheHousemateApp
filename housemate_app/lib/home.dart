4489eb4ecabc7f76bfc3ca700bfa2017c70eab7a
Matt <matthewstanding12@gmail.com>
2025-05-04 17:22:43 +0100

    added a drawer

--- housemate_app/lib/home.dart
+++ housemate_app/lib/home.dart
@@ -11,6 +11,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List<List<String>> pages = [
+    ["Calender", '/calenderPage'],
+    ["Shopping List",'/shoppingList'],
+    ["Group Chat",' '],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,41 +25,142 @@ class _MyHomePageState extends State<MyHomePage> {
           centerTitle: true,
           title: const Text("Dweller Home Page"),
         ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  border: Border(bottom: const BorderSide(color: Colors.black,width: 1))
+                
+                ),
+                padding: const EdgeInsets.all(8),
+                height: 90,
+                child: Row(
+                children: [
+                  IconButton( // profile button
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/userProfile');
+                    }, 
+                    icon: const Icon(Icons.person),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(color: Colors.black,width: 1),
+                      shape: const CircleBorder(),
+                      
+                    ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  Text(
+                    'Garry Giggles',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  )
+                ],
+              ),),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: const Icon(Icons.house_outlined),
+                          title:  const Text('House Info'),
+                          onTap: (){},
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: const Icon(Icons.hourglass_bottom),
+                          title:  const Text('Action Log'),
+                          onTap: (){},
+                        ),
+                      ),
+                ],
+                ),
+              ),
+
+//  DRAWER -    B O T T O M    B A R 
+// ---------------------------------------------------------------------------
+
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  border: Border(top: const BorderSide(color: Colors.black,width: 1))
+                
+                ),
+                padding: const EdgeInsets.all(8),
+                height: 60,
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton( // settings button
+                    onPressed: (){}, 
+                    icon: const Icon(Icons.settings),
+                    
+                    ),
+                    IconButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    }, 
+                    icon: const Icon(Icons.arrow_back),
+                    
+                    ),
+                    
+                  ],
+                ),
+              )
+            ],
+          ),
+
+        ),
+
         body: Center(
             child: SizedBox.square(
                 dimension: MediaQuery.of(context).size.height,
-                child:GridView.count(
-                   //Stack(alignment: Alignment.topLeft, children: [
-                   crossAxisCount: 2,
-                   mainAxisSpacing: 40,
-                   crossAxisSpacing: 30,
-                   childAspectRatio: 4/1,
-                   children: [
-                  ElevatedButton(
-                      onPressed: null, child: Text("House Info")),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/userProfile');
-                      },
-                      child: const Text("Profile")),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/shoppingList');
-                      },
-                      child: const Text("Shopping List")),
-
-                          // CALENDER
-                  ElevatedButton( 
-                      onPressed: (){
-                        Navigator.pushNamed(context, '/calenderPage');
-                      }, 
-                      child: Text("Calender")),
-                  ElevatedButton(
-                      onPressed: null, child: Text("Chore List")),
-                  ElevatedButton(
-                      onPressed: null, child: Text("Group Chat")),
-                  ElevatedButton(
-                      onPressed: null, child: Text("Action Log")),
-                ]))));
+                child: Container(
+                  //width: 100,
+                  padding: const EdgeInsets.only(top: 60,bottom: 40,left: 20,right: 20),
+                  //color: Colors.blue,
+                  child: ListView.builder(
+                    itemCount: pages.length,
+                    shrinkWrap: true,
+                    itemBuilder: (
+                      BuildContext context,
+                      int index,
+                    ) {
+                      
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, pages[index][1]);
+                        },
+                        child: 
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              pages[index][0],
+                              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                                fontSize: 18,
+                            
+                              ),
+                                                    ),
+                          ),),
+                      );
+                          
+                      
+                            
+                    }
+                    ),
+                ),
+              
+                ),
+                  ));
   }
 }
--- housemate_app/lib/utils/theme.dart
+++ housemate_app/lib/utils/theme.dart
@@ -6,10 +6,15 @@ ThemeData defaultTheme = ThemeData(
   ),
   textTheme: const TextTheme(
     displayMedium: TextStyle(
-      fontSize: 32, 
+      fontSize: 20, 
       fontWeight: FontWeight.normal, 
       fontFamily: 'Inter',
-      )
+      ),
+    displayLarge: TextStyle(
+      fontSize: 22, 
+      fontWeight: FontWeight.bold, 
+      fontFamily: 'Inter',
+      ),
   ),
   appBarTheme: const AppBarTheme(
     backgroundColor: Colors.grey,
@@ -17,6 +22,26 @@ ThemeData defaultTheme = ThemeData(
       fontWeight: FontWeight.bold, 
       fontSize: 22,
       fontFamily: 'Inter',
+    )
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.black,
+      textStyle: const TextStyle(
+        fontWeight: FontWeight.w600,
+        fontFamily: 'Inter',
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(0),
+        side: BorderSide(color: Colors.black,width: 1)
+      ),
+      padding: EdgeInsets.all(10),
+    )
+  ),
+  iconButtonTheme: IconButtonThemeData(
+    style: IconButton.styleFrom(
+      foregroundColor: Colors.black,
+      //backgroundColor: Colors.white,
     )
   )
 
