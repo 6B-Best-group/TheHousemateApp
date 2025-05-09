8a7a12418ec63151ad9b5286d95482538a26e178
fishedishe <UP2246632@myport.ac.uk>
2025-05-09 16:13:13 +0100

    design (mostly) complete

--- housemate_app/lib/rota.dart
+++ housemate_app/lib/rota.dart
@@ -9,34 +9,77 @@ class Rota extends StatefulWidget {
   State<Rota> createState() => _RotaState();
 }
 
-class _RotaState extends State<Rota> {
+class CustomListItem extends StatelessWidget {
+  const CustomListItem({
+    super.key,
+    required this.thumbnail,
+    required this.choreName,
+    required this.assignee,
+    required this.nextAssignee,
+  });
+
+  final Widget thumbnail;
+  final String choreName;
+  final String assignee;
+  final String nextAssignee;
+
   @override
   Widget build(BuildContext context) {
-    final reversedActionsList = actionsList.reversed.toList();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(flex: 1, child: thumbnail),
+          Expanded(
+            flex: 1,
+            child: Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(child: Text(choreName, style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0))),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text('Assigned to: $assignee', style: const TextStyle(fontSize: 15.0)),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text('Next: $nextAssignee', style: const TextStyle(fontSize: 10.0)),
+        ],
+      ),
+    )
+          ),
+          const Icon(Icons.more_vert, size: 16.0),
+        ],
+      ),
+    );
+  }
+}
 
+class _RotaState extends State<Rota> {
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Rota"),
+        title: const Text("Weekly Rota"),
       ),
-      body: ListView.builder(
-        itemCount: reversedActionsList.length,
-        itemBuilder: (context, i) {
-
-          return Column(
-            children: <Widget>[
-              ListTile(
-                leading: CircleAvatar(
-                  child: Text(reversedActionsList[i].notificationName[0]),
-                ),
-                title: Text(reversedActionsList[i].notificationName),
-                subtitle: Text(reversedActionsList[i].notificationDetail),
-                trailing: Text(reversedActionsList[i].dateAdded.toString().substring(0, 16)),
-              ),
-              const Divider(height: 0),
-            ],
-          );
-      },
-    ),
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        itemExtent: 106.0,
+        children: <CustomListItem>[
+          CustomListItem(
+            choreName: 'Clean Kitchen Sides',
+            assignee: '${currentUser.getFirstName()} ${currentUser.getLastName()}',
+            nextAssignee: '${currentUser.getFirstName()} ${currentUser.getLastName()}',
+            thumbnail: Container(decoration: const BoxDecoration(color: Colors.green)),
+          ),
+          CustomListItem(
+            choreName: 'Vacuum Living Room Floor',
+            assignee: 'Dash',
+            nextAssignee: '884000',
+            thumbnail: Container(decoration: const BoxDecoration(color: Colors.red)),
+          ),
+        ],
+      ),
     );
   }
 }
